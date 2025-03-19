212de37f34c772b242708e1e15e6fb4a24e0d7e1
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-03-19 12:56:39 -0500

    feat:cambios

--- src/test/java/co/edu/eci/arep/x/XApplicationTests.java
+++ src/test/java/co/edu/eci/arep/x/XApplicationTests.java
@@ -1,13 +1,8 @@
 package co.edu.eci.arep.x;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
